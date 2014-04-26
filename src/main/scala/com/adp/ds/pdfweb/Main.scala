b352c46a51103b4d51ffee22370274b8d8dd0219
Fawad Halim <fawad@fawad.net>
2014-04-25 21:36:00 -0500

    Some cleanup to write multiple images

--- src/main/scala/com/adp/ds/pdfweb/Main.scala
+++ src/main/scala/com/adp/ds/pdfweb/Main.scala
@@ -1,23 +1,26 @@
 package com.adp.ds.pdfweb
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.LoggerFactory
 import org.apache.pdfbox.pdmodel.{PDPage, PDDocument}
-import java.io.File
+import java.io.{FileOutputStream, File}
+import resource.managed
 import scala.collection.JavaConversions._
-import javax.imageio.ImageIO
-import java.awt.image.BufferedImage
+import org.apache.pdfbox.util.ImageIOUtil
 
 object Main extends App {
   val logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld1")
-  val pdfName = "c:/temp/Used Motor Vehicle Llimited Warranty.pdf"
-  logger.debug(s"Converting file ${pdfName}")
+  //val pdfFile = new File("c:/temp/bills.pdf")
+  val pdfFile = new File("c:/temp/Used Motor Vehicle Llimited Warranty.pdf")
+  logger.debug(s"Converting file ${pdfFile.getAbsolutePath}")
 
-  val document = PDDocument.load(new File(pdfName))
+  val document = PDDocument.load(pdfFile)
   for((obj, idx) <- document.getDocumentCatalog.getAllPages zip Range(1,Int.MaxValue)){
     val page = obj.asInstanceOf[PDPage]
     val img = page.convertToImage
-    val fileName = s"c:/temp/image${idx}.png"
-    logger.debug(s"Writing to file ${fileName}")
-    ImageIO.write(img, "png", new File(fileName))
+    val outFile = new File(s"${pdfFile.getAbsolutePath}${idx}.png")
+    logger.debug(s"Writing to file ${outFile.getAbsolutePath}")
+    for(os <- managed(new FileOutputStream(outFile))){
+      ImageIOUtil.writeImage(img, "png", os)
+    }
   }
   println("Done")
 }
