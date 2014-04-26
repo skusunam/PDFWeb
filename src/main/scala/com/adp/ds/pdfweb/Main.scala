0f68b9f92325c523bf5cc30c887b5e7aaf1945b2
Fawad Halim <fawad@fawad.net>
2014-04-26 09:07:01 -0500

    Generate HTML with proper scale

--- src/main/scala/com/adp/ds/pdfweb/Main.scala
+++ src/main/scala/com/adp/ds/pdfweb/Main.scala
@@ -1,26 +1,46 @@
 package com.adp.ds.pdfweb
 import org.slf4j.LoggerFactory
 import org.apache.pdfbox.pdmodel.{PDPage, PDDocument}
-import java.io.{FileOutputStream, File}
+import java.io.{FileWriter, FileOutputStream, File}
 import resource.managed
 import scala.collection.JavaConversions._
 import org.apache.pdfbox.util.ImageIOUtil
 
 object Main extends App {
-  val logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld1")
-  //val pdfFile = new File("c:/temp/bills.pdf")
-  val pdfFile = new File("c:/temp/Used Motor Vehicle Llimited Warranty.pdf")
-  logger.debug(s"Converting file ${pdfFile.getAbsolutePath}")
+  implicit def toFile(path:String) = new File(path)
+
+  val logger = LoggerFactory.getLogger(Main.getClass)
+  val pdfFile = "testFile.pdf"
+  logger.debug(s"Reading file ${pdfFile.getAbsolutePath}")
 
   val document = PDDocument.load(pdfFile)
   for((obj, idx) <- document.getDocumentCatalog.getAllPages zip Range(1,Int.MaxValue)){
     val page = obj.asInstanceOf[PDPage]
+    val baseName = s"${pdfFile.getAbsolutePath.getParent}/${pdfFile.getName.split('.')(0)}$idx"
+    writeImage(page, baseName)
+    writeHtml(page, baseName)
+  }
+
+  def writeHtml(page: PDPage, baseName: String) {
+    val scale = 96.0 / 72.0
+    val width = page.getMediaBox.getWidth * scale
+    val height = page.getMediaBox.getHeight * scale
+    logger.debug(s"Writing to file $baseName.html")
+    for (writer <- managed(new FileWriter(s"$baseName.html"))) {
+      writer.write(
+        s"""<html><head></head><body>
+           <img src="${baseName.getName}.png" style="width:${width}px;height:${height}px" />
+           </body></html>
+        """.stripMargin)
+    }
+  }
+
+  def writeImage(page: PDPage, baseName: String) {
     val img = page.convertToImage
-    val outFile = new File(s"${pdfFile.getAbsolutePath}${idx}.png")
-    logger.debug(s"Writing to file ${outFile.getAbsolutePath}")
-    for(os <- managed(new FileOutputStream(outFile))){
+    val outFile = s"$baseName.png"
+    logger.debug(s"Writing to file $outFile")
+    for (os <- managed(new FileOutputStream(outFile))) {
       ImageIOUtil.writeImage(img, "png", os)
     }
   }
-  println("Done")
 }
